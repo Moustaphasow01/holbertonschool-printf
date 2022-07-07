709eb404ef5ce89cb85c32432fec8b7e23c1b390
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-07-07 04:55:25 -0700

    update print_check

--- print_check.c
+++ print_check.c
@@ -1,36 +1,27 @@
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * print_check - selects the right printing function
+ * depending on the conversion specifier passed to _printf 
+ * @s: character that holds the conversion specifier
+ * prt[] to find a match between the specifier passed to _printf
+ *and the first element of the struct, and then the approriate
+ * printing function
+ *Return: a pointer to the matching printing function
  */
 
-int print_check(const char * const format, ...)
+int (*print_check(char s))(va_list)
 {
 	prt_f prt[] = {
+	        {'i', print_int},
 		{'c', print_char},
+		{'d', print_int},
 		{'s', print_string},
 		{'\0', NULL},
 	};
 
-	int i = 0, count = 0, j;
-	va_list lst;
-
-	va_start(lst, format);
-
-	while (format[i])
-	{
-		j = 0;
-		while (prt[j].c)
-		{
-			if (prt[j].c == format[i])
-			{
-				count += prt[j].f(lst);
-			}
-			j++;
-		}
-		i++;
-	}
-	va_end(lst);
-	return (count);
+	for (i = 0; i < 6; i++)
+	  if (func_arr[i].c == s)
+	    return (func_arr[i].f);
+	return (NULL);
 }
